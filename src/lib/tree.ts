be2cdcdb4d571ab245b401731f856b0a94477da9
mityax <mityax@github.com>
2023-02-05 15:52:36 +0100

    Construct all possible trees

--- src/lib/tree.ts
+++ src/lib/tree.ts
@@ -1,63 +1,88 @@
+import Tree from "./components/Tree.svelte";
 
 export class TreeNode<T> {
     public data: T;
     public left: TreeNode<T> | null = null;
     public right: TreeNode<T> | null = null;
 
-    constructor(data: T) {
+    constructor(data: T, left: TreeNode<T> | undefined = undefined, right: TreeNode<T> | undefined = undefined) {
         this.data = data;
+        this.left = left !== undefined ? left : null;
+        this.right = right !== undefined ? right : null;
+    }
+
+    copy(): TreeNode<T> {
+        return new TreeNode<T>(this.data, this.left?.copy(), this.right?.copy());
     }
 }
 
 
-export function buildTreePreIn<T>(preOrder: T[], inOrder: T[]): TreeNode<T>[] | null {
+export function buildTreePreIn<T>(preOrder: T[], inOrder: T[]): TreeNode<T>[] {
     if (inOrder.length === 0 || preOrder.length === 0) {
-        return null;
+        return [];
     }
 
-    const node = new TreeNode<T>(preOrder[0]);
+    const value = preOrder[0];
 
-    const inIndex = inOrder.indexOf(node.data);
+    const inIndexes = allIndexesOf(inOrder, value);
 
     if (inOrder.length === 1 || preOrder.length === 1) {
-        return node;
+        return [new TreeNode<T>(value)];
     }
 
-    node.left = buildTreePreIn(preOrder.slice(1, inIndex + 1), inOrder.slice(0, inIndex))
-    node.right = buildTreePreIn(preOrder.slice(inIndex + 1), inOrder.slice(inIndex + 1));
+    const res: TreeNode<T>[] = [];
 
-    return node;
+    for (let index of inIndexes) {
+        let leftOptions = buildTreePreIn(preOrder.slice(1, index + 1), inOrder.slice(0, index))
+        let rightOptions = buildTreePreIn(preOrder.slice(index + 1), inOrder.slice(index + 1));
+
+        for (let leftOpt of (leftOptions.length > 0 ? leftOptions : [null])) {
+            for (let rightOpt of (rightOptions.length > 0 ? rightOptions : [null])) {
+                res.push(new TreeNode<T>(value, leftOpt, rightOpt));
+            }
+        }
+    }
+
+    return res;
 }
 
 
-export function buildTreeInPost<T>(inOrder: T[], postOrder: T[]): TreeNode<T>[] | null {
+export function buildTreeInPost<T>(inOrder: T[], postOrder: T[]): TreeNode<T>[] {
     if (inOrder.length === 0 || postOrder.length === 0) {
-        return null;
+        return [];
     }
 
-    const node = new TreeNode<T>(postOrder.at(-1));
+    const value = postOrder.at(-1);
 
-    const inIndex = inOrder.indexOf(node.data);
+    const inIndexes = allIndexesOf(inOrder, value);
 
     if (inOrder.length === 1 || postOrder.length === 1) {
-        return node;
+        return [new TreeNode<T>(value)];
     }
 
-    node.left = buildTreeInPost(inOrder.slice(0, inIndex), postOrder.slice(0, inIndex));
-    node.right = buildTreeInPost(inOrder.slice(inIndex + 1), postOrder.slice(inIndex, -1));
+    const res = [];
 
-    return node;
+    for (let index of inIndexes) {
+        const leftOptions = buildTreeInPost(inOrder.slice(0, index), postOrder.slice(0, index));
+        const rightOptions = buildTreeInPost(inOrder.slice(index + 1), postOrder.slice(index, -1));
+
+        for (let leftOpt of (leftOptions.length > 0 ? leftOptions : [null])) {
+            for (let rightOpt of (rightOptions.length > 0 ? rightOptions : [null])) {
+                res.push(new TreeNode<T>(value, leftOpt, rightOpt));
+            }
+        }
+    }
+
+    return res;
 }
 
 
-// Function to find index of value
-// in arr[start...end]. The function
-// assumes that value is present in in[]
-function search<T>(arr: T[], strt: number, end: number, value: T): number  {
-    let i;
-    for(i = strt; i <= end; i++) {
-        if (arr[i] == value)
-            return i;
+function allIndexesOf<T>(array: T[], value: T): number[] {
+    const res: number[] = [];
+    for(let i=0; i<array.length; i++) {
+        if (array[i] === value) {
+            res.push(i);
+        }
     }
     return res;
 }
